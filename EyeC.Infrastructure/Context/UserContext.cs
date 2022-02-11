32117873727cf0a398297a84f500b6790266d358
Xenophrania <71230567+Xenophrania@users.noreply.github.com>
2022-02-11 13:44:39 +0200

    Added  Infrastructure and Domain Levels

---
+++ EyeC.Infrastructure/Context/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using EyeC.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EyeC.Infrastructure.Context
+{
+    public class UserContext : IdentityDbContext<AppUser>
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
